838b89a0036dc2d408c30bdaf218f05f74de267f
Chuxi Wang <wangchuxi94@gmail.com>
2021-02-17 20:49:45 -0800

    add grid view of the movie

---
+++ flix/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  flix
+//
+//  Created by Mialsy on 2/17/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
